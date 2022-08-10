ae5362f5477074eb8db337b8a5b750eb43d985fd
litrofaq <litrofaq@yandex.ru>
2022-08-10 13:38:49 +0500

    Initial initialization of the project. Added the NUnit and Selenium packages. Added ChromeDriver 104.

---
+++ FirstSeleniumTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumL01;
+
+public class FirstSeleniumTests
+{
+    private ChromeDriver _chromeDriver;
+    
+    [SetUp]
+    public void SetUp()
+    {
+        var options = new ChromeOptions();
+        options.AddArgument("start-maximized");
+        _chromeDriver = new ChromeDriver(options);
+    }
+    
+    [Test]
+    public void Test1()
+    {
+        
+    }
+    
+    [TearDown]
+    public void TearDown()
+    {
+        _chromeDriver.Quit();
+    }
+}
